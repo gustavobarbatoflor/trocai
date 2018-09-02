4b029b52b08cd3eabb481578c3940545a85128a9
gustavobarbatoflor <gustavo.barbato.flor@gmail.com>
2018-09-02 02:46:52 -0300

    CRUDs exemplo funcionais

--- app/home/views.py
+++ app/home/views.py
@@ -1,9 +1,8 @@
 # app/home/views.py
 
-from flask import render_template
-from flask_login import login_required
+from flask import abort, render_template
+from flask_login import current_user, login_required
 from . import home
-
 
 @home.route('/')
 def homepage():
@@ -20,3 +19,14 @@ def dashboard():
     Render the dashboard template on the /dashboard route
     """
     return render_template('dashboard.html', title="Dashboard")
+
+
+# add admin dashboard view
+@home.route('/admin/dashboard')
+@login_required
+def admin_dashboard():
+    # prevent non-admins from accessing the page
+    if not current_user.is_admin:
+        abort(403)
+
+    return render_template('admin_dashboard.html', title="Dashboard")
